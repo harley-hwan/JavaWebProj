18fee7f7cb44d2e163cf659c3c0a15ee984ba569
harley <harley_hwan@naver.com>
2021-11-19 13:39:19 +0900

    update GlassBridge code

--- java_project/오징어게임_다리건너기/GlassBridge.java
+++ java_project/오징어게임_다리건너기/GlassBridge.java
@@ -10,7 +10,7 @@ public class GlassBridge {
 		Random randNum = new Random();
 		Scanner sc = new Scanner(System.in);
 
-		int step = 0;
+		int step = 1;
 		int steps = 16;
 		int choose; // 내 선택
 		int ans; // 유리 징검다리 정답
@@ -19,32 +19,51 @@ public class GlassBridge {
 		System.out.println("여러분은 15개의 징검다리를 건너시면 됩니다.\n");
 		
 		while (true) {
-			step++;
-			
 			if (randNum.nextInt(2) == 0) {
 				ans = 1; // left (왼쪽 징검다리)
 			} else {
 				ans = 2; // right (오른쪽 징검다리)
 			}
 			
-			while (true) {	
+			
+			
+			try {
+				// 숫자를 입력해야 되는데 문자를 입력할 경우 에러나서 프로그램 종료가 됨
+				// 이렇게 예외처리를 하면 된다
 				System.out.println("왼쪽(1) or 오른쪽(2) ?");
-				choose = sc.nextInt();
-				if (choose == 1 || choose == 2)
-					break;
-
-				System.out.println("1 혹은 2 둘 중 하나만 고르시오.");
+				choose = sc.nextInt(); // 베팅한 구슬의 수
+				if (choose != 1 && choose != 2) {
+					System.out.println("1 또는 2만 입력하시오.\n");
+					sc.nextLine();   // 개행문자 처리
+					continue;
+				}
+			} catch (Exception e) {
+				System.out.println("숫자만 입력이 가능 합니다.\n");
+				sc.nextLine();   // 개행문자 처리
+				continue;
 			}
+			
+			
+			
+//			while (true) {	
+//				System.out.println("왼쪽(1) or 오른쪽(2) ?");
+//				choose = sc.nextInt();
+//				if (choose == 1 || choose == 2)
+//					break;
+//
+//				System.out.println("1 혹은 2 둘 중 하나만 고르시오.");
+//			}
 
 			if (ans != choose) {
 				System.out.println("사망!");
 				break;
 			}
-			System.out.println(step + "번째 징검다리를 통과하였습니다.");
+			System.out.println(step + "번째 징검다리를 통과하였습니다.\n");
 			if (step == steps) {
 				System.out.println("당신은 이번 게임에서 생존하였습니다.");
 				break;
 			}
+			step++;
 		}	
 	}
 }
--- java_project/오징어게임_다리건너기/GlassBridge2.java
+++ java_project/오징어게임_다리건너기/GlassBridge2.java
@@ -11,44 +11,46 @@ public class GlassBridge2 {
 		Random randNum = new Random();
 		Scanner sc = new Scanner(System.in);
 		
-		int stepGlass = 10;		// 징검다리
+		int stepGlass = 16;		// 징검다리
 		int countDead = 0;		// 생존자 수
 		int people = 10;		// 참가자 수
 		int step = 0;			// 내 직전의 사람이 건넌 다리 수
 		
 		System.out.println("이번 게임은 다리 건너기 게임입니다.");
-		System.out.println("여러분은 총 10개의 징검다리를 건너시면 됩니다.");
+		System.out.println("여러분은 총 " + stepGlass + "개의 징검다리를 건너시면 됩니다.");
 		
-		for (int i = 0; i < people; i++) {
-			int ans = randNum.nextInt(2);
-			int choice = randNum.nextInt(2);
-			if (choice == 0) {
+		while(true) {
+			int ans = randNum.nextInt(2);		// 0: 왼쪽, 1: 오른쪽 (정답)
+			int choice = randNum.nextInt(2);	// 0: 왼쪽, 1: 오른쪽 (참가자의 선택)
+			if (choice == 0) {			// 왼쪽 징검다리를 고름
 				System.out.println("왼쪽 징검다리를 선택합니다.");
-				if (choice == ans) {
+				if (choice == ans) {	// 왼쪽을 골랐는데 왼쪽이 정답
 					System.out.println("생존했습니다.");
-				}
-				else {
-					System.out.println("사망했습니다.");
+				}	
+				else {					// 왼쪽을 골랐는데 오른쪽이 정답
 					countDead++;
+					System.out.println(countDead + "번째 참가자가 사망했습니다.");
+					
 				}
 			}
-			else {
+			else {						// 오른쪽 징검다리를 고름
 				System.out.println("오른쪽 징검다리를 선택합니다.");
-				if (choice == ans) {
+				if (choice == ans) {	// 오른쪽을 골랐는데 오른쪽이 정답
 					System.out.println("생존했습니다.");
 				}
-				else {
-					System.out.println("사망했습니다.");
+				else {					// 오른쪽을 골랐는데 왼쪽이 정답
 					countDead++;
+					System.out.println(countDead + "번째 참가자가 사망했습니다.");
 				}
 			}
 			step++; // 앞 사람이 사망하든 생존하든 징검다리를 건넌건 동일하기 때문에 한 사람의 턴이 지날 때마다 step을 증가
 			
 			System.out.println(step + "번째 다리를 건넜습니다.\n");
 			
-			if (step == stepGlass) break;	
+			if (step == stepGlass || countDead == people) break;	
 		}
 		
-		System.out.println("이번 게임에서는 생존한 참가자는 " + (people - countDead) + "명입니다.");
+		// 5번째 사람이 통과했다면, 그 뒤의 사람들도 다 통과
+		System.out.println("이번 게임에서는 생존한 참가자는 " + (people - countDead) + "명입니다.");	
 	}
 }
