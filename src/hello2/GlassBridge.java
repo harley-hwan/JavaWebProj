5c80e5596c49206aa861220836a233333dea3835
harley <harley_hwan@naver.com>
2021-11-19 09:21:55 +0900

    Create GlassBridge.java

--- src/hello2/GlassBridge.java
+++ src/hello2/GlassBridge.java
@@ -6,25 +6,45 @@ public class GlassBridge {
 
 	public static void main(String[] args) {
 		// 오징어 게임 - 다리 건너기
-		
+
 		Random randNum = new Random();
 		Scanner sc = new Scanner(System.in);
-		
+
+		int step = 0;
 		int steps = 16;
-		boolean choose;
+		int choose; // 내 선택
+		int ans; // 유리 징검다리 정답
 		
-		if (randNum.nextInt(2) == 0) {
-			choose = false;		// left	 (왼쪽 징검다리 고름)
-		} else {
-			choose = true;		// right (오른쪽 징검다리 고름)
-		}
+		System.out.println("이번 게임은 징검다리 건너기입니다.");
+		System.out.println("여러분은 15개의 징검다리를 건너시면 됩니다.\n");
 		
-		if (choose) {
+		while (true) {
+			if (randNum.nextInt(2) == 0) {
+				ans = 1; // left (왼쪽 징검다리)
+			} else {
+				ans = 2; // right (오른쪽 징검다리)
+			}
 			
-		}
-		
-		
+			while (true) {
+				step++;
+				
+				System.out.println("왼쪽(1) or 오른쪽(2) ?");
+				choose = sc.nextInt();
+				if (choose == 1 || choose == 2)
+					break;
 
+				System.out.println("1 혹은 2 둘 중 하나만 고르시오.");
+			}
+
+			if (ans != choose) {
+				System.out.println("사망!");
+				break;
+			}
+			System.out.println(step + "번째 징검다리를 통과하였습니다.");
+			if (step == steps) {
+				System.out.println("당신은 이번 게임에서 생존하였습니다.");
+				break;
+			}
+		}	
 	}
-
 }
